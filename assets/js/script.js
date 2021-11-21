32c603243ab02a377df2e2a8b16430d1916732ee
Kerry Eames <Kerryeames@live.com>
2021-11-21 14:54:05 -0700

    added basic HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// HTML needs <header>
+// <form> with <h1> and search <button>
+// column list of <buttons>
+// both in aside to the left of screen
+// forecast card <div> with <h1> and a <ul> with 4 <li>
+// <div> with <h3> and 5 JS generated cards built similarly
+// both stacked and taking up the rest of the screen
