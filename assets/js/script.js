20a8511a807a7bd8f4a6f5eb932dc9146e262105
Kerry Eames <Kerryeames@live.com>
2021-11-21 21:12:43 -0700

    added local storage functionality for entered cities. Cities cap at 8 items and remove the oldest result upon more searches. cannot submit empty form.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,54 @@
-// HTML needs <header>
-// <form> with <h1> and search <button>
-// column list of <buttons>
-// both in aside to the left of screen
-// forecast card <div> with <h1> and a <ul> with 4 <li>
-// <div> with <h3> and 5 JS generated cards built similarly
-// both stacked and taking up the rest of the screen
+var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+var cityList = $("#city-list");
+var searchForm = $("#search-form");
+
+function saveCities(event) {
+  event.preventDefault();
+  var cityInput = $("#city-input").val();
+  if (cityInput === "") {
+    return;
+  } else {
+    storedCities.push(cityInput);
+    localStorage.setItem("cities", JSON.stringify(storedCities));
+    $("#city-input").val("");
+    renderCities();
+  }
+}
+
+function renderCities() {
+  if (storedCities.length >= 8) {
+    storedCities.shift(7);
+    cityList.text("");
+    for (var i = 0; i < storedCities.length; i++) {
+      var cityListItem = $(
+        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
+      );
+      cityListItem.text(storedCities[i]);
+      cityListItem.attr("data-index", i);
+      cityList.prepend(cityListItem);
+    }
+    cityList.text("");
+    for (var i = 0; i < storedCities.length; i++) {
+      var cityListItem = $(
+        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
+      );
+      cityListItem.text(storedCities[i]);
+      cityListItem.attr("data-index", i);
+      cityList.prepend(cityListItem);
+    }
+  } else {
+    cityList.text("");
+    for (var i = 0; i < storedCities.length; i++) {
+      var cityListItem = $(
+        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
+      );
+      cityListItem.text(storedCities[i]);
+      cityListItem.attr("data-index", i);
+      cityList.prepend(cityListItem);
+    }
+  }
+}
+
+searchForm.on("submit", saveCities);
+
+renderCities();
