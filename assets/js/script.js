6b58a566a480ea37cbb7ef0d5788fd26a9ca6ba9
Kerry Eames <Kerryeames@live.com>
2021-11-21 22:43:02 -0700

    added functionality for selecting previous searches, does not work, no explanation for why. Began api call code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 var cityList = $("#city-list");
 var searchForm = $("#search-form");
+var searchedCity = $("#searched-city");
+
+// City search history
 
 function saveCities(event) {
   event.preventDefault();
@@ -21,16 +24,7 @@ function renderCities() {
     cityList.text("");
     for (var i = 0; i < storedCities.length; i++) {
       var cityListItem = $(
-        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
-      );
-      cityListItem.text(storedCities[i]);
-      cityListItem.attr("data-index", i);
-      cityList.prepend(cityListItem);
-    }
-    cityList.text("");
-    for (var i = 0; i < storedCities.length; i++) {
-      var cityListItem = $(
-        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
+        '<button class="btn btn-secondary special-button mt-1 mb-2" id="searched-city"></button>'
       );
       cityListItem.text(storedCities[i]);
       cityListItem.attr("data-index", i);
@@ -40,7 +34,7 @@ function renderCities() {
     cityList.text("");
     for (var i = 0; i < storedCities.length; i++) {
       var cityListItem = $(
-        '<button class="btn btn-secondary special-button mt-1 mb-2"></button>'
+        '<button class="btn btn-secondary special-button mt-1 mb-2" id="searched-city"></button>'
       );
       cityListItem.text(storedCities[i]);
       cityListItem.attr("data-index", i);
@@ -52,3 +46,51 @@ function renderCities() {
 searchForm.on("submit", saveCities);
 
 renderCities();
+
+// Search previous results
+
+function saveSearchedCities(event) {
+  event.preventDefault();
+  var cityInput = searchedCity.text();
+  console.log(cityInput);
+  storedCities.push(cityInput);
+  localStorage.setItem("cities", JSON.stringify(storedCities));
+  searchedCity.val("");
+  renderCities();
+}
+
+searchedCity.click(saveSearchedCities);
+
+// API call to get weather info
+
+function getWeather() {
+  var newCity = storedCities[fruits.length];
+  var cityUrl =
+    "api.openweathermap.org/data/2.5/forecast/daily?q=" +
+    newCity +
+    "&cnt=6&APPID=b838f6a74f084064314505e05fecb924";
+  fetch(cityUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var todaysWeather = document.getElementById("todaysWeather");
+        var weekWeather = document.getElementById("weekWeather");
+        var cityDate = document.createElement("h3");
+        var forecastDetails = document.createElement("ul");
+        var tempDetails = document.createElement("li");
+        var windDetails = document.createElement("li");
+        var humDetails = document.createElement("li");
+        var uvDetails = document.createElement("li");
+        var weekCard = document.createElement("div");
+        var weekCardDetail = document.createElement("div");
+        var dateOnly = document.createElement("h3");
+      }
+    });
+}
